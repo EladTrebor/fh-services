648ad86b66c59bb85d59afbe11dae25a5d1c70db
Zac-AND-Digital <zac.king@and.digital>
2024-09-23 12:22:52 +0100

    wip: get to delete service from service detail

--- src/ui/manage-ui/src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/delete-service.cshtml.cs
+++ src/ui/manage-ui/src/FamilyHubs.ServiceDirectory.Admin.Web/Pages/manage-services/delete-service.cshtml.cs
@@ -1,20 +1,17 @@
 using FamilyHubs.ServiceDirectory.Admin.Core.ApiClient;
-using FamilyHubs.SharedKernel.Identity;
-using FamilyHubs.SharedKernel.Identity.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages;
+namespace FamilyHubs.ServiceDirectory.Admin.Web.Pages.manage_services;
 
-// TODO: Move into a proper place
 public class DeleteService : PageModel
 {
     private readonly IServiceDirectoryClient _serviceDirectoryClient;
     private readonly IReferralService _referralServiceClient;
 
     // TODO: Note while building: 1 has 0 requests, 664 has 1 request
-    public long ServiceId { get; set; } = 1; // TODO: This will probably be passed in from the Service Detail page bit
-    public string? ServiceName { get; set; } // TODO: Possibly also this ^
+    public long ServiceId { get; set; }
+    public string? ServiceName { get; set; }
 
     public DeleteService(IServiceDirectoryClient serviceDirectoryClient, IReferralService referralServiceClient)
     {
@@ -25,25 +22,29 @@ public class DeleteService : PageModel
     private async Task<bool> IsOpenConnectionRequests() =>
         await _referralServiceClient.GetReferralsCountByServiceId(ServiceId) > 0;
 
-    public void OnPost()
+    public async Task<IActionResult> OnPostAsync()
     {
-        // TODO: Check for Open Connection Requests
         // TODO: Redirect to Error Page if True
         // TODO: Delete the Service if False
+
+        if (await IsOpenConnectionRequests())
+        {
+            return RedirectToPage("Welcome"); // TODO: Open Connection Requests Error Page
+        }
 
         throw new NotImplementedException();
     }
 
-    public async Task<IActionResult> OnGetAsync()
+    public async Task<IActionResult> OnGetAsync(long serviceId)
     {
-        // TODO: Check for Open Connection Requests [ DONE ]
-        // TODO: Redirect to Error Page if True
-        // TODO: Continue loading Delete Service Page if False
-        // TODO: Test ID's in the .cshtml
+        ServiceId = serviceId;
 
-        if (await IsOpenConnectionRequests()) return RedirectToPage("Welcome"); // TODO: Error Page
+        if (await IsOpenConnectionRequests())
+        {
+            return RedirectToPage("Welcome"); // TODO: Open Connection Requests Error Page
+        }
 
-        ServiceName = (await _serviceDirectoryClient.GetServiceById(ServiceId)).Name;
+        ServiceName = (await _serviceDirectoryClient.GetServiceById(serviceId)).Name;
 
         return Page();
     }
