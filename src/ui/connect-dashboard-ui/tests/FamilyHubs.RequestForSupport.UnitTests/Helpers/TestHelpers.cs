6840f05d8cc421c54363bf241ad06e8f32458541
Stuart.Maskell <stuart.maskell@education.gov.uk>
2024-10-09 10:17:27 +0100

    Add MockClient helper - To be moved to sharedl ibrary later

---
+++ src/ui/connect-dashboard-ui/tests/FamilyHubs.RequestForSupport.UnitTests/Helpers/TestHelpers.cs
@@ -0,0 +1,35 @@
+using System.Net;
+
+namespace FamilyHubs.RequestForSupport.UnitTests.Helpers;
+
+// TODO: This is duplicated accross projects, when their all updated with NSubstitute, createa shared project for this
+public static class TestHelpers
+{
+    public static HttpClient GetMockClient(string content, HttpStatusCode statusCode = HttpStatusCode.OK)
+    {
+        var mockHttpMessageHandler = new CustomHttpMessageHandler(content, statusCode);
+
+        var client = new HttpClient(mockHttpMessageHandler)
+        {
+            BaseAddress = new Uri("https://localhost")
+        };
+        return client;
+    }
+
+    /// <summary>
+    /// Custom HttpMessageHandler to return a response message with the content
+    /// </summary>
+    private class CustomHttpMessageHandler(string content, HttpStatusCode statusCode) : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var responseMessage = new HttpResponseMessage(statusCode)
+            {
+                Content = new StringContent(content),
+                RequestMessage = request
+            };
+
+            return Task.FromResult(responseMessage);
+        }
+    }
+}
